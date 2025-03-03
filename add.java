9a8f760d5d82873d8c36d2d57fdfbee15115de69
mahi <mahivpatel2006@gmail.com>
2025-03-03 12:46:51 +0530

    Add add.java files

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add
+{
+    public static void main(string[]args){
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        syatem.out.println("sum of a and b is:" + c);
+    }
+}
