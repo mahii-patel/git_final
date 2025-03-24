f940dd86f5b75515a5cd811cf90e9db06bc5761d
mahii-patel <mahivpatel2006@gmail.com>
2025-03-24 13:31:29 +0530

    Update add.java

--- add.java
+++ add.java
@@ -3,7 +3,7 @@ public class add
     public static void main(string[]args){
         int a = 10;
         int b = 20;
-        int c = a+b;
-        syatem.out.println("sum of a and b is:" + c);
+        int c = ab;
+        syatem.out.println("sum of a and b is:"  c);
     }
 }
